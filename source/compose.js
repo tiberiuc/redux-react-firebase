49034eba86eb7de668244a8ab71066a6e98948df
Tibi Craciun <tibi@happysoft.ro>
2016-01-26 20:59:03 +0200

    Fixed remove helper

--- source/compose.js
+++ source/compose.js
@@ -31,7 +31,7 @@ export default (url, config) => {
 
     const set = (path, value, onComplete) => ref.child(path).set(value, onComplete)
     const push = (path, value, onComplete) => ref.child(path).push(value, onComplete)
-    const remove = (path, value, onComplete) => ref.child(path).remove(value, onComplete)
+    const remove = (path, onComplete) => ref.child(path).remove(onComplete)
     const login = credentials => Actions.login(dispatch, firebase, credentials)
     const logout = () => Actions.logout(dispatch, firebase)
     const createUser = (credentials, profile) => Actions.createUser(dispatch, firebase, credentials, profile)
