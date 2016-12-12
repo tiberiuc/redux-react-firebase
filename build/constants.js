0d2c867eaac124ba8eae979a3308417b24175ba6
Rahav lussato <rahav@yallo.com>
2016-12-12 22:15:55 +0200

    no message

---
+++ build/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var prefix = '@@reactReduxFirebase/';
+
+module.exports = {
+  SET: prefix + 'SET',
+  SET_PROFILE: prefix + 'SET_PROFILE',
+  LOGIN: prefix + 'LOGIN',
+  LOGOUT: prefix + 'LOGOUT',
+  LOGIN_ERROR: prefix + 'LOGIN_ERROR',
+  //  NO_VALUE: `${prefix}NO_VALUE`,
+  START: actionsPrefix + 'START',
+  INIT_BY_PATH: actionsPrefix + 'INIT_BY_PATH'
+};
